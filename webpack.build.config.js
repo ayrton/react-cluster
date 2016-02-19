626999af44289b5cb1ac1b1462c5b5565fb768d0
Ayrton De Craene <ayrton.decraene@gmail.com>
2016-02-19 14:16:42 -0800

    Properly export

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -9,7 +9,18 @@ module.exports = {
     library : 'ReactCluster',
   },
   externals: {
-    'react': 'React'
+    'react': {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM'
+    }
   },
   module: {
     loaders: [
